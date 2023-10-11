2239e676102be594790a948250322e8fe065b9d6
xh3b4sd <xh3b4sd@gmail.com>
2023-10-12 00:04:56 +0200

    allow calling rescue.Fake() (#23)

--- factory.go
+++ factory.go
@@ -1,7 +1,18 @@
 package rescue
 
-import "github.com/xh3b4sd/rescue/engine"
+import (
+	"github.com/xh3b4sd/redigo"
+	"github.com/xh3b4sd/rescue/engine"
+)
 
 func Default() Interface {
-	return engine.New(engine.Config{})
+	return engine.New(engine.Config{
+		Redigo: redigo.Default(),
+	})
+}
+
+func Fake() Interface {
+	return engine.New(engine.Config{
+		Redigo: redigo.Fake(),
+	})
 }
